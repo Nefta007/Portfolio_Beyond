bf9c048096ccf7571183c73b652f1e6f18de8128
Neftali <nperc001@ucr.edu>
2023-12-21 15:46:22 -0800

    added resume file

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,10 @@
+export default function Resume() {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <p>
+                Please click on link to see my Resume <a href="https://docs.google.com/document/d/1_QG-nuqkYNGtVjbDza8OAcXao_kakvHn/edit?usp=sharing&ouid=107910275213586943995&rtpof=true&sd=true">click here</a>
+            </p>
+        </div>
+    )
+}
