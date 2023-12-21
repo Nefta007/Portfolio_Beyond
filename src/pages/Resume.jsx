fcb2eb1884103c288abe540cb73a42dc445ba162
Neftali <nperc001@ucr.edu>
2023-12-21 15:56:34 -0800

    created resume file

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,9 +2,71 @@ export default function Resume() {
     return (
         <div>
             <h1>Resume</h1>
-            <p>
+            {/* <p>
                 Please click on link to see my Resume <a href="https://docs.google.com/document/d/1_QG-nuqkYNGtVjbDza8OAcXao_kakvHn/edit?usp=sharing&ouid=107910275213586943995&rtpof=true&sd=true">click here</a>
-            </p>
+            </p> */}
+            <section>
+                <h2>Objective</h2>
+                <p>
+                    Seeking entry-level position related to electrical engineering to further
+                    develop my knowledge and skills, as well as further developing experience
+                    of technical application of current skills known.
+                </p>
+            </section>
+            <section>
+                <h2>Educatoin</h2>
+                <p>University of California, Riverside              September 2019- March 2025
+                    Bachelors of Science, Electrical Engineering
+                </p>
+            </section>
+            <section>
+                <h2>Skills</h2>
+                <p>
+                    LABORATORY: Data Analysis, Circuit safety, Circuit Analysis, Building a Circuit
+                    OTHER SOFTWARE: Microsoft Office: Word, Excel, PowerPoint, Matlab, C++, LTSpiceXVII
+                    LANGUAGE: Fluent English and Spanish
+                    DESIGN: Autodesk Inventor
+                </p>
+            </section>
+            <section>
+                <h2>Soft Skills</h2>
+                <p>
+                    Communication		            Willingness to Learn			Adaptability
+                    Teamwork				        Problem Solving 		    	Dependable
+                </p>
+            </section>
+            <section>
+                <h2>Academic Experience</h2>
+                <p>
+                    Worked with breadboard to create various forms of circuits and applied ohms law as well as Kirchhoff`s current and voltage laws
+                    Used LTSpice to simulate circuits and compared used it as a form of comparison to outcomes from circuits we physically made
+                    Used MATLAB to calculate theoretical values of Kirchhoff`s current and voltage laws
+                    Assembled and using mesh analysis to determine current flowing through a loop
+                    Worked with oscilloscopes, multimeters, and power supplies in order to measure power delivered to a load by using RMS voltage and current
+                    Built circuits in order to analyze active filters where we saw a high voltage at low frequencies and low voltages at higher frequencies
+                    Utilizing circuits, we were able to see the affects of frequency on phasor values as well as calculate gain
+                    Applied superposition and Thevenin theorem in AC circuits using input op-amp, signal generator, oscilloscope, resistors and capacitors.
+                </p>
+            </section>
+            <section>
+                <h2>Work Experience</h2>
+                <p>
+                    <h3>99 Cents Store</h3>					
+                    July 2019 to present
+                    Managed money from both customers and employees
+                    Provided great customer service in high pressure environment
+                    Adapted to various roles at a moment`s notice
+
+                    <h3>Photophobia</h3>					    					      
+                    August 2019 to June 2020
+                    Created a welcoming atmosphere for clients
+                    Responsible for setting backdrop and props
+                    Assisted in sales and distribution
+                    Handled assembly and maintenance of camera
+                    Arranged client poses
+
+                </p>
+            </section>
         </div>
     )
 }
