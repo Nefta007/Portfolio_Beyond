a66b45796364896c36c75099d822f6080d98d206
Neftali <nperc001@ucr.edu>
2023-12-21 15:37:04 -0800

    added starter code and pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                Hello, my name is Neftali Percastegui, and I am a Full stack web developer. I am 22 years old and currently in college getting a
+                degree in electrical engineering. I am proficient at using Reach, SQL, Javascript, CSS, HTML, and database usage.
+                Through this website I hope to show how I have grown as a coder and web developer, as well as show samples with
+                my portfolio.
+            </p>
+        </div>
+    );
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,21 @@
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <div>
+                <div>
+                    <h3>Name</h3>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <h3>Name</h3>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <h3>Name</h3>
+                    <input type="text"/>
+                </div>
+            </div>
+        </div>
+    )
+}
