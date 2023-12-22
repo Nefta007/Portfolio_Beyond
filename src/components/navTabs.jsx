1b726675f9e046356715e593a2064ab208daf4e2
Neftali <nperc001@ucr.edu>
2023-12-21 17:07:14 -0800

    created navbar

---
+++ src/components/navTabs.jsx
@@ -0,0 +1,35 @@
+import { Link, useLocation } from 'react-router-dom';
+
+function navTab(){
+    const currentPage = useLocation().pathname;
+    return(
+        <nav>
+            <ul className='nav nav-tab'>
+                <li className='nav-item'>
+                    <Link to='/' 
+                    className={currentPage ==='/' ? 'nav-link active' : 'nav-link'}>
+                        About
+                    </Link>
+                </li>
+                <li className='/Portfolio'>
+                    <Link to='/Portfolio' 
+                    className={currentPage ==='/Portfolio' ? 'nav-link active' : 'nav-link'}>
+                        Portfolio
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/Contact' 
+                    className={currentPage ==='/Contact' ? 'nav-link active' : 'nav-link'}>
+                        Contact
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/Resume' 
+                    className={currentPage ==='/Resume' ? 'nav-link active' : 'nav-link'}>
+                        Resume
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
